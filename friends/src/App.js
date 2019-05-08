b5456e6c70125dd617f420e9837f86a08bd490c1
Doug <me@douglasbowen.com>
2019-05-08 16:35:02 -0400

    add friends to state object from server.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import FriendsComponent from "./components/FriendsComponent";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { friends: [] };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(res => {
+        console.log(res);
+        this.setState({ friends: res.data });
+        console.log(this.state.friends);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">New App Header</header>
+        <FriendsComponent />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ friends/src/components/FriendsComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FriendsComponent() {
+  return (
+    <div>
+      <p>Friends go here</p>
+    </div>
+  );
+}
+
+export default FriendsComponent;
